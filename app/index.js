a3b8adb9649d92234fa7e76929f4e88ead4cb5f9
Mixer97 <169163595+Mixer97@users.noreply.github.com>
2026-06-04 23:38:19 +0200

    feat: add match API endpoints

--- app/index.js
+++ app/index.js
@@ -10,6 +10,7 @@ const { verifyToken } = require("./middleware/auth");
 const authRoutes = require("./routes/auth.route");
 const fieldRoutes = require("./routes/field.route");
 const tournamentRoutes = require("./routes/tournament.route");
+const matchRoutes = require("./routes/match.route");
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/fields", fieldRoutes);
 app.use("/api/tournaments", tournamentRoutes);
+app.use("/api/matches", matchRoutes);
 
 // base test
 app.get("/", (req, res) => {
